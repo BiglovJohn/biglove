33d1746c4db8fab7e10ac421db4c4d48481c9154
Evgeniy Biglov <biglov.evgen@gmail.com>
2022-12-28 13:19:05 +0300

    22.12.28 updated

--- app_rules/views.py
+++ app_rules/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
+from .services import rules_service as rs
+
 
 def our_values_page(request):
     return render(request, 'app_rules/our_values.html')
 
 
 def rules_page(request):
-    content = open('documents/content.txt', 'r', encoding='utf-8')
-    lines = content.readlines()
-    return render(request, 'app_rules/rules.html', {'lines': lines})
+    rules = rs.RuleService()
+    return render(request, 'app_rules/rules.html', {'rules': rules})
 
 
 def privacy_policy_page(request):
